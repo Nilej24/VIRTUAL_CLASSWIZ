9d317fdd42b2abdbf1e25656d438d098c84a2cb4
Nijel <nmanalo543@gmail.com>
2022-02-05 17:48:42 +0000

    added all the files

---
+++ script.js
@@ -0,0 +1,16 @@
+function operate(op, a, b) {
+
+	switch(op) {
+	
+		case "+":
+			return a + b;
+		case "-":
+			return a - b;
+		case "*":
+			return a * b;
+		case "/":
+			return a / b;
+	
+	}
+
+}
